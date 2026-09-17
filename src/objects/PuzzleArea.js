6619b9e2f8d56d8ed81b34884954038b2e2e81cb
Peter <petermhajj@gmail.com>
2026-09-16 23:28:05 -0400

    fix: added outline to PuzzleArea Octogon and reorentated it

--- src/objects/PuzzleArea.js
+++ src/objects/PuzzleArea.js
@@ -20,10 +20,22 @@ export class PuzzleArea extends THREE.Group {
       const radius = (pixelSize / 2) * worldUnitsPerPixel;
 
       // Circle with 8 triangle segments to create an octogon shape
-      const geometry = new THREE.CircleGeometry(radius, 8); 
+      const geometry = new THREE.CircleGeometry(radius, 8);
       const material = new THREE.MeshBasicMaterial({color: 0x1F2430});
       const octogon = new THREE.Mesh(geometry,material);
 
+      // Outline of the play area, using a scaled up octogon
+      const outlineScale = 1.15;
+      const outlineGeometry = new THREE.CircleGeometry(radius * outlineScale, 8);
+      const outlineMaterial = new THREE.MeshBasicMaterial({color: 0x4A5568});
+      const outline = new THREE.Mesh(outlineGeometry, outlineMaterial);
+      // Outline is below the original in the z axis
+      outline.position.z = -0.01;
+
+      this.add(outline);
       this.add(octogon);
+
+      // Rotate by pi/8 because pi = 180 degrees
+      this.rotation.z = Math.PI / 8;
    }
 }
